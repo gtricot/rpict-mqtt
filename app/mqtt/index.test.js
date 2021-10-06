25d95f79f1cc399d9466d4c286839b7bb1dab91e
Guillaume Tricot <gtricot79@gmail.com>
2021-10-06 07:27:47 +0200

    fix unit tests lint

--- app/mqtt/index.test.js
+++ app/mqtt/index.test.js
@@ -7,18 +7,6 @@ mqttAsync.connectAsync = jest.fn(() => {
 });
 const mqtt = require('./index');
 
-const sample = {
-    ADCO: { raw: '012345678912', value: 12345678912 },
-    OPTARIF: { raw: 'BASE', value: 'BASE' },
-    ISOUSC: { raw: '45', value: 45 },
-    BASE: { raw: '022304568', value: 22304568 },
-    PTEC: { raw: 'TH..', value: 'TH' },
-    IINST: { raw: '003', value: 3 },
-    IMAX: { raw: '090', value: 90 },
-    PAPP: { raw: '00720', value: 720 },
-    HHPHC: { raw: 'A', value: 'A' },
-};
-
 test('connect should be called as expected', async () => {
     const moduleToTest = rewire('./index');
     moduleToTest.__set__('client', {
@@ -38,14 +26,3 @@ test('disconnect should be called as expected', async () => {
     await moduleToTest.disconnect();
     expect(moduleToTest.__get__('client').end).toHaveBeenCalledTimes(1);
 });
-
-// test('publishFrame should be called as expected', () => {
-//     const moduleToTest = rewire('./index');
-//     moduleToTest.__set__('client', {
-//         publish: jest.fn(() => {
-//         }),
-//     });
-//     moduleToTest.__set__('discoveryConfigurationPublished', true);
-//     moduleToTest.publishFrame(sample);
-//     expect(moduleToTest.__get__('client').publish).toHaveBeenCalledWith('rpict/012345678912', JSON.stringify(sample));
-// });
