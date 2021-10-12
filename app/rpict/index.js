c1b462898315d87e72d94142c11a9e112e61b34d
Guillaume Tricot <gtricot79@gmail.com>
2021-10-12 14:59:48 +0200

    rename config FRACTION_DIGITS to PRECISION and INVERT_NEGATIVE_VALUES to ABSOLUTE_VALUES

--- app/rpict/index.js
+++ app/rpict/index.js
@@ -4,10 +4,10 @@ const events = require('events');
 const log = require('../log');
 const {
     baudRate,
-    fractionDigits,
+    precision,
     serial,
     deviceMapping,
-    invertNegativeValues,
+    absoluteValues,
     sensorValueThreshold,
 } = require('../config');
 
@@ -17,13 +17,13 @@ let deviceMappingJson;
 function parseDataFromTemplateParams(data, configItem) {
     let returnValue;
     const valueType = deviceMappingJson[configItem].type;
-    log.trace(`Parsing value ${data} with type ${valueType} for config item ${configItem}`);
+    log.debug(`Parsing value ${data} with type ${valueType} for config item ${configItem}`);
     switch (valueType) {
     case 'float':
 
         // Parse value
-        returnValue = Number(parseFloat(data).toFixed(fractionDigits));
-        log.trace(`Parsed float value ${returnValue} for config item ${configItem} with ${fractionDigits} fraction digits`);
+        returnValue = Number(parseFloat(data).toFixed(precision));
+        log.debug(`Parsed float value ${returnValue} for config item ${configItem} with ${precision} decimals`);
 
         // Check for NaN value
         if (Number.isNaN(returnValue)) {
@@ -32,13 +32,13 @@ function parseDataFromTemplateParams(data, configItem) {
         }
 
         // Invert negative value if parameterized
-        if (returnValue < 0 && invertNegativeValues) {
-            log.trace(`Inverting negative return value ${returnValue} for config item ${configItem}`);
+        if (returnValue < 0 && absoluteValues) {
+            log.debug(`Inverting negative return value ${returnValue} for config item ${configItem}`);
             returnValue = -returnValue;
         }
         // Set value to 0.0 if inferior to threshold
         if (returnValue < sensorValueThreshold) {
-            log.trace(`Return value ${returnValue} inferior to threshold ${sensorValueThreshold} for config item ${configItem}. Returning 0.0 instead`);
+            log.debug(`Return value ${returnValue} inferior to threshold ${sensorValueThreshold} for config item ${configItem}. Returning 0.0 instead`);
             returnValue = 0.0;
         }
 
@@ -56,13 +56,13 @@ function parseDataFromTemplateParams(data, configItem) {
         }
 
         // Invert negative value if parameterized
-        if (returnValue < 0 && invertNegativeValues) {
-            log.trace(`Inverting negative return value ${returnValue} for config item ${configItem}`);
+        if (returnValue < 0 && absoluteValues) {
+            log.debug(`Inverting negative return value ${returnValue} for config item ${configItem}`);
             returnValue = -returnValue;
         }
         // Set value to 0 if inferior to threshold
         if (returnValue < sensorValueThreshold) {
-            log.trace(`Return value ${returnValue} inferior to threshold ${sensorValueThreshold} for config item ${configItem}. Returning 0 instead`);
+            log.debug(`Return value ${returnValue} inferior to threshold ${sensorValueThreshold} for config item ${configItem}. Returning 0 instead`);
             returnValue = 0;
         }
 
@@ -74,7 +74,7 @@ function parseDataFromTemplateParams(data, configItem) {
         returnValue = data;
     }
 
-    log.trace(`Parsed value ${returnValue} from raw value ${data} with type ${valueType} for config item ${configItem}`);
+    log.debug(`Parsed value ${returnValue} from raw value ${data} with type ${valueType} for config item ${configItem}`);
     return returnValue;
 }
 
