486bc83c9a45d3897a6329679c791a34c91ca76a
Guillaume Tricot <gtricot79@gmail.com>
2025-09-23 05:41:26 +0200

    Adds local development environment bash config
    Introduces a bash script to configure the local development environment.
    
    This allows developers to easily set environment variables
    needed for running the application locally, defining MQTT
    connection details, a sensor threshold, and logging configurations.

---
+++ app/setEnv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export MQTT_URL=http://localhost:1883
+export MQTT_USER=test
+export MQTT_PASSWORD=test
+export SENSOR_VALUE_THRESHOLD=10
+export LOG_FORMAT=text
+export LOG_LEVEL=DEBUG
